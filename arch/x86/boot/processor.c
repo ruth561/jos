c867ee1aa9982763d035ce4839219d4feda53a92
takumi jin <primenumber_2_3_5@yahoo.co.jp>
2024-03-20 16:35:27 +0900

    Implemented get_rflags

--- arch/x86/boot/processor.c
+++ arch/x86/boot/processor.c
@@ -4,9 +4,30 @@
 #include "panic.h"
 
 
+static u64 get_rflags()
+{
+	u64 rflags;
+	asm volatile (
+		"pushfq\n\t"
+		"popq	%0"
+		: "=r" (rflags)
+		:
+		: "memory"
+	);
+	return rflags;
+}
+
 void processor_init()
 {
+	int log_level = set_log_level(LOG_LEVEL_DEBUG);
 	INFO("started processor_init()");
+
+	u64 rflags = get_rflags();
+	DEBUG("RFLAGS: 0x%x", rflags);
+	while (1);
+
 	INFO("ended processor_init()");
+	set_log_level(log_level);
+
 	PANIC("...");
 }
