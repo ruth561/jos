67f59efe619a59f71a89cac33df8e5a41098cd7f
takumi jin <primenumber_2_3_5@yahoo.co.jp>
2024-03-20 16:55:47 +0900

    Implemented get_cr0

--- arch/x86/boot/processor.c
+++ arch/x86/boot/processor.c
@@ -2,7 +2,10 @@
 #include "assert.h"
 #include "logger.h"
 #include "panic.h"
+#include "type.h"
 
+
+#define RFLAGS_F_VM	(1ULL << 17)
 
 static u64 get_rflags()
 {
@@ -17,13 +20,31 @@ static u64 get_rflags()
 	return rflags;
 }
 
+#define CR0_F_PE	(1ULL << 0)
+
+static u64 get_cr0()
+{
+	u64 cr0;
+	asm volatile (
+		"movq	%%cr0, %0"
+		: "=r" (cr0)
+	);
+	return cr0;
+}
+
 void processor_init()
 {
 	int log_level = set_log_level(LOG_LEVEL_DEBUG);
 	INFO("started processor_init()");
 
+	u64 cr0 = get_cr0();
+	CHECK(cr0 & CR0_F_PE); // not Real mode
+	DEBUG("CR0: 0x%lx", cr0);
+	
 	u64 rflags = get_rflags();
-	DEBUG("RFLAGS: 0x%x", rflags);
+	CHECK(!(rflags & RFLAGS_F_VM)); // not Virtual-8086 mode
+	DEBUG("RFLAGS: 0x%lx", rflags);
+
 	while (1);
 
 	INFO("ended processor_init()");
