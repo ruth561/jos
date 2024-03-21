775a577d08a4a23aae830aef3aa03b73846c50d8
takumi jin <primenumber_2_3_5@yahoo.co.jp>
2024-03-21 11:18:18 +0900

    Implemented set_rflags and check_cpuid_features
    プロセッサがcpuid命令に対応しているかどうかを確認する関数。
    EFLAGS.IDフラグをセットできるかどうかで検証することができる。

--- arch/x86/boot/processor.c
+++ arch/x86/boot/processor.c
@@ -6,6 +6,7 @@
 
 
 #define RFLAGS_F_VM	(1ULL << 17)
+#define RFLAGS_F_ID	(1ULL << 21)
 
 static u64 get_rflags()
 {
@@ -18,6 +19,27 @@ static u64 get_rflags()
 		: "memory"
 	);
 	return rflags;
+}
+
+static void set_rflags(u64 rflags)
+{
+	asm volatile (
+		"push	%0\n\t"
+		"popfq"
+		:: "r" (rflags)
+		: "memory"
+	);
+}
+
+// プロセッサがCPUIDの機能を持っているのかどうかを確かめる。
+static bool check_cpuid_feature()
+{
+	u64 rflags = get_rflags();
+	DEBUG("check_cpuid_feature: rflags = %lx", rflags);
+	set_rflags(rflags | RFLAGS_F_ID);
+	rflags = get_rflags();
+	DEBUG("check_cpuid_feature: rflags = %lx", rflags);
+	return (rflags & RFLAGS_F_ID) == RFLAGS_F_ID;
 }
 
 #define CR0_F_PE	(1ULL << 0)
@@ -44,6 +66,8 @@ void processor_init()
 	u64 rflags = get_rflags();
 	CHECK(!(rflags & RFLAGS_F_VM)); // not Virtual-8086 mode
 	DEBUG("RFLAGS: 0x%lx", rflags);
+
+	CHECK(check_cpuid_feature());
 
 	while (1);
 
